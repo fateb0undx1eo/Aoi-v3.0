21d2e36d171230871e0bc4bcffc77a13fd3a23fd
fateb0undx1eo <explainersenpai@gmail.com>
2026-05-29 00:41:48 +0530

    random 2.7

--- landing/src/pages/index.tsx
+++ landing/src/pages/index.tsx
@@ -415,51 +415,214 @@ function ReviewCard({
 
 function AoiCatMascot({ className = "" }: { className?: string }) {
   return (
-    <svg
-      viewBox="0 0 190 150"
-      role="img"
-      aria-label="AOI cat mascot"
-      className={`cat-mascot ${className}`}
-    >
-      {/* Body */}
-      <path id="path1" className="cat-body" d="M50 105c0-25 20-45 45-45s45 20 45 45c0 20-10 28-45 28s-45-8-45-28Z" />
-      
-      {/* Ears */}
-      <path id="path6" className="cat-ear" d="M60 70 68 28l28 32Z" />
-      <path id="path7" className="cat-ear" d="M100 68 122 25l12 40Z" />
-      
-      {/* Whiskers */}
-      <path id="path8" className="cat-whisker" d="M72 90 35 85m40 15-40 3m68-10 38-5m-38 15 38 2" />
-      
-      {/* Tail */}
-      <path id="tail" className="cat-tail" d="M130 95c18-3 28-14 24-26-3-9-14-12-22-6-5 4-5 13 1 17 5 3 12 1 14-5" />
-      
-      {/* Head */}
-      <path id="head" className="cat-head" d="M42 80c0-24 20-42 53-42s53 18 53 42c0 28-20 44-53 44s-53-7-53-44Z" />
-      
-      {/* Muzzle/Face */}
-      <path id="muzzle" className="cat-muzzle" d="M78 92c6-7 28-7 35 0 6 6 2 18-17 18s-24-11-18-18Z" />
-      
-      {/* Eyes (with eyelids) */}
-      <ellipse id="lefteyelid" className="cat-eye" cx="78" cy="75" rx="8" ry="12" />
-      <ellipse id="righteyelid" className="cat-eye" cx="112" cy="75" rx="8" ry="12" />
-      
-      {/* Pupils */}
-      <ellipse id="ellipse15" className="cat-pupil" cx="81" cy="76" rx="3" ry="7" />
-      <ellipse id="ellipse16" className="cat-pupil" cx="109" cy="76" rx="3" ry="7" />
-      
-      {/* Nose */}
-      <path id="nose" className="cat-nose" d="M91 96h10l-5 6Z" />
-      
-      {/* Mouth */}
-      <path id="mouth" className="cat-mouth" d="M97 103c-3 5-10 6-15 2m15-2c3 5 10 6 15 2" />
-      
-      {/* Collar */}
-      <path id="collar" className="cat-collar" d="M70 118c15 10 40 10 55 0" />
-      
-      {/* Tag */}
-      <circle id="tag" className="cat-tag" cx="95" cy="128" r="6" />
-    </svg>
+    <div className={`footer-cat-container ${className}`}>
+      <div className="mouse-detector">
+        <div className="cat">
+          {/* Sleeping Zzz */}
+          <div className="sleep-symbol">
+            <span>Z</span>
+            <span>z</span>
+            <span>z</span>
+          </div>
+
+          {/* The Cat SVG */}
+          <div className="thecat">
+            <svg
+              width="45.952225mm"
+              height="35.678726mm"
+              viewBox="0 0 45.952225 35.678726"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs />
+              <g transform="translate(-121.80376,-101.90461)">
+                {/* Cat Body */}
+                <path
+                  style={{ display: 'inline', fill: '#000000', stroke: 'none', strokeWidth: '0.264583' }}
+                  d="m 144.95859,104.74193 c 6.01466,-2.1201 14.02915,-0.85215 17.62787,2.77812 3.59872,3.63027 2.91927,7.6226 -0.0661,11.80703 -2.98542,4.18443 -9.54667,3.58363 -15.1474,3.43959 -5.60073,-0.14404 -10.30411,-0.0586 -11.67474,-3.9026 7.85671,-2.22341 3.24576,-12.00205 9.26042,-14.12214 z"
+                  id="path1"
+                />
+
+                {/* Right Ear */}
+                <path
+                  style={{ display: 'inline', fill: '#000000', stroke: 'none', strokeWidth: '0.264583' }}
+                  d="m 156.30732,121.30486 c 0,0 -3.82398,2.52741 -4.14054,3.7997 -0.31656,1.2723 0.31438,2.18109 0.95701,2.55128 0.64264,0.3702 1.59106,-0.085 2.13559,-0.75306 0.54452,-0.6681 1.5629,-2.25488 2.47945,-3.20579 0.91654,-0.95091 2.96407,-2.74361 2.96407,-2.74361 l 0.73711,-3.60348 z"
+                  id="path2"
+                />
+
+                {/* Left Ear */}
+                <path
+                  style={{ display: 'inline', fill: '#000000', stroke: 'none', strokeWidth: '0.264583' }}
+                  d="m 136.93356,123.08347 c 0,0 -3.20149,3.2804 -3.24123,4.59088 -0.0397,1.31049 0.60411,1.83341 1.3106,2.05901 0.7065,0.22559 1.60304,-0.55255 1.99363,-1.32084 0.39056,-0.76832 1.14875,-2.30337 2.04139,-3.29463 0.89264,-0.99126 3.37363,-3.37561 3.37363,-3.37561 l -1.30007,-3.61169 z"
+                  id="path3"
+                />
+
+                {/* Whiskers Left Bottom */}
+                <path
+                  style={{ display: 'inline', fill: '#000000', stroke: 'none', strokeWidth: '0.264583' }}
+                  d="m 130.12859,121.60522 c -2.15849,1.92962 -3.38576,3.23532 -3.61836,4.5256 -0.23257,1.2903 0.0956,1.80324 0.76105,2.13059 0.66549,0.32733 1.66701,-0.31006 2.16665,-1.01233 0.49961,-0.70231 1.04598,-1.14963 2.83575,-3.05671 1.78977,-1.90708 5.91823,-3.27102 5.91823,-3.27102 l -0.75313,-3.99546 c 0,0 -5.15171,2.7497 -7.31019,4.67933 z"
+                  id="path4"
+                />
+
+                {/* Face/Body Front */}
+                <path
+                  id="path5"
+                  style={{ display: 'inline', fill: '#000000', stroke: 'none', strokeWidth: '0.292536' }}
+                  d="m 147.59927,113.85404 c 0.68896,4.40837 -4.04042,7.93759 -10.51533,8.9455 -6.47491,1.00791 -12.24344,-0.88717 -12.9324,-5.29555 -0.68895,-4.40838 3.44199,-9.94186 9.9169,-10.94977 6.47491,-1.0079 12.84186,2.89144 13.53083,7.29982 z"
+                />
+
+                {/* Left Head Top */}
+                <path
+                  style={{ display: 'inline', fill: '#000000', stroke: 'none', strokeWidth: '0.264583' }}
+                  d="m 126.36446,111.82609 c 0,0 -2.37067,-6.28072 -0.86724,-7.10855 1.50342,-0.82783 5.87139,3.72617 5.87139,3.72617 z"
+                  id="path6"
+                />
+
+                {/* Right Head Top */}
+                <path
+                  style={{ display: 'inline', fill: '#000000', stroke: 'none', strokeWidth: '0.264583' }}
+                  d="m 143.50182,108.85407 c 0,0 -0.0544,-6.71302 -1.75519,-6.94283 -1.70081,-0.22982 -4.13211,5.59314 -4.13211,5.59314 z"
+                  id="path7"
+                />
+
+                {/* Whiskers Left */}
+                <g id="g25" style={{ display: 'inline' }}>
+                  <path
+                    style={{ fill: 'none', stroke: '#000000', strokeWidth: '0.529167' }}
+                    d="m 125.27102,116.06007 -2.97783,-1.05373"
+                    id="path8"
+                  />
+                  <path
+                    style={{ fill: 'none', stroke: '#000000', strokeWidth: '0.529167' }}
+                    d="m 124.91643,116.80991 -2.84808,0.0754"
+                    id="path9"
+                  />
+                  <path
+                    style={{ fill: 'none', stroke: '#000000', strokeWidth: '0.529167' }}
+                    d="m 124.97798,118.00308 -2.53111,0.5156"
+                    id="path10"
+                  />
+                </g>
+
+                {/* Whiskers Right */}
+                <g
+                  id="g13"
+                  transform="rotate(-23.188815,49.755584,71.047761)"
+                  style={{ display: 'inline', fill: 'none', stroke: '#000000' }}
+                >
+                  <path style={{ strokeWidth: '0.529167' }} d="m 121.77448,146.87682 3.00963,-0.95912" id="path11" />
+                  <path style={{ strokeWidth: '0.529167' }} d="m 122.10521,147.63749 2.84427,0.16537" id="path12" />
+                  <path style={{ strokeWidth: '0.529167' }} d="m 122.00599,148.82812 2.51354,0.59531" id="path13" />
+                </g>
+
+                {/* Eyes (Closed - Eyelids) */}
+                <g id="lefteyelid" style={{ display: 'inline' }}>
+                  <ellipse
+                    style={{ fill: '#000000', stroke: 'none', strokeWidth: '0.529167' }}
+                    id="path21"
+                    cx="131.94429"
+                    cy="114.29948"
+                    rx="3.1571214"
+                    ry="3.2155864"
+                  />
+                  <path
+                    style={{ fill: '#000000', stroke: '#ffffff', strokeWidth: '0.529167' }}
+                    d="m 129.32504,114.80228 c 2.54908,-1.14592 4.60706,-0.65481 4.60706,-0.65481"
+                    id="path22"
+                  />
+                </g>
+
+                <g id="righteyelid" style={{ display: 'inline' }}>
+                  <ellipse
+                    style={{ fill: '#000000', stroke: 'none', strokeWidth: '0.529167' }}
+                    id="ellipse22"
+                    cx="139.07704"
+                    cy="113.0834"
+                    rx="3.1571214"
+                    ry="3.2155864"
+                  />
+                  <path
+                    style={{ fill: '#000000', stroke: '#ffffff', strokeWidth: '0.529167' }}
+                    d="m 136.48089,113.70683 c 2.48528,-1.2784 4.56624,-0.89621 4.56624,-0.89621"
+                    id="path23"
+                  />
+                </g>
+
+                {/* Open Eyes (When Awake) */}
+                <g id="eyesdown">
+                  <ellipse
+                    style={{ fill: '#ffffff', stroke: 'none', strokeWidth: '0.529167' }}
+                    id="path26"
+                    cx="139.12122"
+                    cy="113.61373"
+                    rx="1.8686198"
+                    ry="2.0422525"
+                  />
+                  <ellipse
+                    style={{ fill: '#000000', stroke: 'none', strokeWidth: '0.597086' }}
+                    id="ellipse25"
+                    cx="112.24622"
+                    cy="139.77037"
+                    rx="1.0380507"
+                    ry="1.3097118"
+                    transform="matrix(0.98048242,-0.19660678,0.20800608,0.97812753,0,0)"
+                  />
+                  <ellipse
+                    style={{ fill: '#f9f9f9', stroke: 'none', strokeWidth: '0.184905' }}
+                    id="ellipse26"
+                    cx="112.37342"
+                    cy="139.28929"
+                    rx="0.32146212"
+                    ry="0.40558979"
+                    transform="matrix(0.98048242,-0.19660678,0.20800608,0.97812753,0,0)"
+                  />
+                  <ellipse
+                    style={{ fill: '#ffffff', stroke: 'none', strokeWidth: '0.529167' }}
+                    id="ellipse27"
+                    cx="131.994"
+                    cy="114.92011"
+                    rx="1.8686198"
+                    ry="2.0422525"
+                  />
+                  <ellipse
+                    style={{ fill: '#000000', stroke: 'none', strokeWidth: '0.597086' }}
+                    id="ellipse28"
+                    cx="105.00267"
+                    cy="139.64998"
+                    rx="1.0380507"
+                    ry="1.3097118"
+                    transform="matrix(0.98048242,-0.19660678,0.20800608,0.97812753,0,0)"
+                  />
+                  <ellipse
+                    style={{ fill: '#f9f9f9', stroke: 'none', strokeWidth: '0.184905' }}
+                    id="ellipse29"
+                    cx="105.12987"
+                    cy="139.1689"
+                    rx="0.32146212"
+                    ry="0.40558979"
+                    transform="matrix(0.98048242,-0.19660678,0.20800608,0.97812753,0,0)"
+                  />
+                </g>
+
+                {/* Short Tail */}
+                <path
+                  id="tail"
+                  style={{ display: 'inline', fill: '#000000', stroke: 'none', strokeWidth: '0.529167' }}
+                  d="m 163.77708,109.27292 c 4.36563,2.71198 4.26447,17.63497 3.70417,21.03437 -0.5603,3.3994 -1.86906,4.06275 -4.53099,4.49791 -5.87463,0.96037 -8.39724,-5.87134 -5.7547,-5.72161 2.64254,0.14973 3.15958,3.46446 5.95314,2.05052 2.79356,-1.41394 -1.42214,-13.46068 -1.42214,-13.46068 z"
+                />
+
+                {/* Long Tail (Interaction) */}
+                <path
+                  id="longtail"
+                  style={{ display: 'inline', fill: '#000000', stroke: 'none', strokeWidth: '0.529167' }}
+                  d="m 164.24062,110.09354 -2.10788,6.5381 c 0,0 0.84017,12.88397 0.35269,20.95169 h 4.78291 c 0.83489,-8.63528 0.13334,-24.78453 -3.02772,-27.48979 z"
+                />
+              </g>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
@@ -489,22 +652,26 @@ export default function LandingPage() {
       <SiteNavbar />
 
       <main id="top" className="relative z-10">
-        <section className="public-section aoi-grid-shell relative isolate overflow-hidden px-4 pb-14 pt-8 text-foreground sm:px-6 lg:px-8">
-          <div className="relative mx-auto grid min-h-[calc(100svh-9rem)] max-w-7xl items-center gap-8 px-1 pb-10 pt-6 sm:px-0 lg:grid-cols-[0.9fr_1.1fr] lg:gap-10 lg:pt-8">
-            <Reveal className="relative">
+        <section className="public-section aoi-grid-shell relative isolate overflow-visible px-4 pb-14 pt-8 text-foreground sm:px-6 lg:px-8">
+          <div className="relative mx-auto max-w-7xl px-1 pb-10 pt-6 sm:px-0 lg:pt-8">
+            <Reveal className="relative text-center">
               <div className="inline-flex items-center gap-2 rounded-lg border border-border bg-card px-4 py-2 text-xs uppercase tracking-[0.22em] text-muted-foreground">
                 <Sparkles className="h-4 w-4 text-primary" />
                 Better-for-your-server Discord bot
               </div>
-              <h1 className="mt-5 max-w-2xl text-4xl leading-[1.02] tracking-normal sm:text-5xl lg:text-[3.9rem] xl:text-[4.45rem]">
-                Fuel your Discord
-                <span className="block text-primary">community</span>
-              </h1>
-              <p className="mt-5 max-w-lg text-sm leading-7 text-foreground/82 sm:text-base">
+              <div className="relative mt-5 mx-auto">
+                <h1 className="text-4xl leading-[1.02] tracking-normal sm:text-5xl lg:text-[3.9rem] xl:text-[4.45rem]">
+                  Fuel your Discord
+                  <span className="relative block text-primary">
+                    community
+                  </span>
+                </h1>
+              </div>
+              <p className="mt-5 mx-auto max-w-lg text-sm leading-7 text-foreground/82 sm:text-base">
                 Clean moderation, fast automations, slash commands, and server tools in one sharp AOI control stack.
               </p>
 
-              <div className="mt-8 flex flex-col gap-4 sm:flex-row">
+              <div className="mt-8 flex flex-col items-center gap-4 sm:flex-row sm:justify-center">
                 <MagneticButton href={dashboardUrl}>
                   Open Dashboard
                   <ArrowRight className="h-4 w-4" />
@@ -515,31 +682,24 @@ export default function LandingPage() {
                 </MagneticButton>
               </div>
 
-              <div className="mt-7 grid gap-3 sm:grid-cols-3">
-                {heroStats.map((stat, index) => (
-                  <motion.div
-                    key={stat.label}
-                    className="lux-surface rounded-xl px-4 py-4"
-                    initial={reducedMotion ? false : { opacity: 0, y: 16 }}
-                    whileInView={reducedMotion ? undefined : { opacity: 1, y: 0 }}
-                    viewport={{ once: true, amount: 0.2 }}
-                    transition={{ duration: 0.55, delay: 0.12 + index * 0.08, ease: [0.16, 1, 0.3, 1] }}
-                  >
-                    <div className="text-[11px] uppercase tracking-[0.24em] text-muted-foreground">{stat.label}</div>
-                    <div className="mt-2 card-heading text-2xl text-foreground">{stat.value}</div>
-                  </motion.div>
-                ))}
-              </div>
-            </Reveal>
-
-            <Reveal className="relative flex min-h-[22rem] items-center justify-center lg:min-h-[30rem]">
-              <div className="mascot-stage relative flex aspect-square w-full max-w-[26rem] items-center justify-center rounded-[2rem]">
-                <AoiCatMascot className="relative z-10 w-[84%]" />
-                <div className="absolute bottom-8 left-8 rounded-full bg-primary px-4 py-2 text-xs font-bold text-primary-foreground">
-                  /moderate
+              <div className="relative mt-7">
+                <div className="absolute -top-32 left-0 w-40 sm:w-48 z-20 pointer-events-auto">
+                  <AoiCatMascot className="w-full" />
                 </div>
-                <div className="absolute right-8 top-9 rounded-full border border-border bg-card px-4 py-2 text-xs font-bold text-foreground">
-                  18+ modules
+                <div className="grid gap-3 sm:grid-cols-3">
+                  {heroStats.map((stat, index) => (
+                    <motion.div
+                      key={stat.label}
+                      className="lux-surface rounded-xl px-4 py-4"
+                      initial={reducedMotion ? false : { opacity: 0, y: 16 }}
+                      whileInView={reducedMotion ? undefined : { opacity: 1, y: 0 }}
+                      viewport={{ once: true, amount: 0.2 }}
+                      transition={{ duration: 0.55, delay: 0.12 + index * 0.08, ease: [0.16, 1, 0.3, 1] }}
+                    >
+                      <div className="text-[11px] uppercase tracking-[0.24em] text-muted-foreground">{stat.label}</div>
+                      <div className="mt-2 card-heading text-2xl text-foreground">{stat.value}</div>
+                    </motion.div>
+                  ))}
                 </div>
               </div>
             </Reveal>
